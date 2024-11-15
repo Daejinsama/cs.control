5c8e3bc940404dd6c633881cf4e9189887afbed2
Daejinsama <styner07@outlook.com>
2024-11-15 15:05:05 +0900

    1. base form 추가

---
+++ com.outlook@styner07.cs.control/DjsmBaseForm.cs
@@ -0,0 +1,21 @@
+﻿namespace com.outlook_styner07.cs.control
+{
+    public partial class DjsmBaseForm : Form
+    {
+        public DjsmBaseForm()
+        {
+            InitializeComponent();
+        }
+
+        public const int WS_CAPTION = 0x00c00000;
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams param = base.CreateParams;
+                param.Style = param.Style & ~WS_CAPTION;
+                return param;
+            }
+        }
+    }
+}
