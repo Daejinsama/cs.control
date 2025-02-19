5203137900bf952516ae932c77e7f5d9fd64b3d1
Daejinsama <styner07@outlook.com>
2025-02-19 15:51:11 +0900

    1. imagePanel 메모리 누수 수정

--- com.outlook@styner07.cs.control/Container/DjsmImagePanel.cs
+++ com.outlook@styner07.cs.control/Container/DjsmImagePanel.cs
@@ -58,15 +58,29 @@ namespace com.outlook_styner07.cs.control.Container
             get { return _image; }
             set
             {
-                if (_image == null && value != null)
+                try
                 {
-                    _newWidth = value.Width;
-                    _newHeight = value.Height;
+                    if (_image != null)
+                    {
+                        _image.Dispose();   
+                        _image = null;
+                    }
+
+                    if (_image == null && value != null)
+                    {
+                        _newWidth = value.Width;
+                        _newHeight = value.Height;
+                    }
+
+                    _image = value;
+
+                    Invalidate();
                 }
-
-                _image = value;
-
-                Invalidate();
+                finally
+                {
+                    //GC.Collect();
+                    //GC.SuppressFinalize(this);
+                }
             }
         }
 
